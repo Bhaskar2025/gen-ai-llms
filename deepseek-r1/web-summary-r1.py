d621a3d77d99e7a86795e14e5baaf92e6252fc66
bnaruka <bhaskar.naruka@gmail.com>
2025-02-02 21:10:31 +0530

    Code to connect to DeepSeek R1 API

---
+++ deepseek-r1/web-summary-r1.py
@@ -0,0 +1,45 @@
+import os
+
+import openai
+from dotenv import load_dotenv
+from openai import OpenAI
+
+from webscrapper import website
+from webscrapper.website import Website
+
+load_dotenv(override=True)
+# Set up OpenAI API key
+#openai.api_key = os.environ.get("OPENAI_API_KEY")
+base_url = "https://api.deepseek.com"
+
+API_KEY = os.environ.get("DEEPSEEK_API_KEY")
+client = openai.OpenAI(api_key=API_KEY, base_url=base_url)
+
+# Define system and user prompts
+system_prompt = ("You are an assistant that analyzes the contents of a website "
+                 "and provides a detailed summary, ignoring text that might be navigation related."
+                 "Respond in markdown.")
+
+def user_prompt_generator(website):
+    user_prompt = "The contents of this website is as follows; \
+    please provide a detailed summary of this website in markdown. \
+    If it includes news or announcements, then summarize these too.\n\n"
+    user_prompt += website.text
+    return user_prompt
+
+
+
+# Send a request to OpenAI's GPT model
+site = Website("https://globe24news.com/")
+
+response = openai.chat.completions.create(
+        model = "deepseek-reasoner",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt_generator(site)}
+        ],
+        max_tokens=1000
+)
+
+# Print the model's response
+print(response.choices[0].message.content)
